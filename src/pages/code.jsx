4742bb2a83de6fb404f1736cc9c3dc72dcfbfe12
William Capozzoli <thinkocapo@gmail.com>
2020-04-17 19:18:25 -0700

    react-syntax-highlighter on Code page

--- src/pages/code.jsx
+++ src/pages/code.jsx
@@ -3,12 +3,40 @@ import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import { Header } from 'components';
 import { Layout, Container } from 'layouts';
-// import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown'
 
 // import barranquilla from './road-barranquilla.jpg'
 
 // const input = `
+// ## Test
+// <code>
+// function heyo() {
+//     console.log('lets go')
+// }
+// </code>
 // `
+
+// const input = `
+// ## Test
+// \`\`\`javascript
+// function heyo() {
+//     console.log('lets go')
+// }
+// \`\`\`
+// `
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { coy } from "react-syntax-highlighter/dist/styles/prism";
+
+// const Prism = require('prismjs');
+import Prism from 'prismjs'
+
+// The code snippet you want to highlight, as a string
+const code = `var data = 1;`;
+
+// Returns a highlighted HTML string
+// const html = Prism.highlight(code, Prism.languages.javascript, 'javascript');
+// {html}
 
 const Code = center => (
   <Layout>
@@ -17,9 +45,14 @@ const Code = center => (
     <Container left={center}>
       <h2>Coming Soon...</h2>
       <p>For now see my code at <a href="https://github.com/thinkocapo">https://github.com/thinkocapo</a></p>
-      
-      {/* <h2 style={{ 'color': 'blue' }}>Books</h2> */}
+  
+      <SyntaxHighlighter language={'javascript'}>
+        {code}
+      </SyntaxHighlighter>
 
+
+      {/* <ReactMarkdown source={input}/> */}
+      {/* <h2 style={{ 'color': 'blue' }}>Books</h2> */}
       <br></br>
 
     </Container>
